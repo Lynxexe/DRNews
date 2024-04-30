3b0c174129e07dd9e29df57bbfbdcc02b680b8d4
danie <danie@DESKTOP-VPF1S1R>
2024-04-30 11:18:26 +0200

    Colour changes on main page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,43 @@
-﻿namespace DRNews
+﻿using DRNews.Components.Pages;
+using Microsoft.Maui.Controls;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace DRNews
 {
     public partial class MainPage : ContentPage
     {
+        private static string selectedCategory = "SenesteNyt";
+        private Frame lastSelectedFrame;
+        private Color mainColor = Color.FromHex("#1976D2"); // Main color
+        private Color selectedColor = Color.FromHex("#FFA500");
         public MainPage()
         {
             InitializeComponent();
+            lastSelectedFrame = SenesteNytFrame;
+            SenesteNytFrame.BackgroundColor = selectedColor;
+        }
+        private void SenesteNytLabel_Tapped(object sender, EventArgs e)
+        {
+            
+            UpdateButtonAppearance(SenesteNytFrame);
+        }
 
+        private void IndlandLabel_Tapped(object sender, EventArgs e)
+        {
+            UpdateButtonAppearance(IndlandFrame);
+        }
+
+        private void UdlandLabel_Tapped(object sender, EventArgs e)
+        {
+            UpdateButtonAppearance(UdlandFrame);
+        }
+
+        private void UpdateButtonAppearance(Frame currentFrame)
+        {
+            lastSelectedFrame.BackgroundColor = mainColor;
+
+            currentFrame.BackgroundColor = selectedColor;
+            lastSelectedFrame = currentFrame;
         }
     }
 }
